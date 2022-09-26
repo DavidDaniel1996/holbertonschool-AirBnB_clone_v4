b140d0c8207c720d1dcfa09e77d7238541c8d1dd
David <4292@holbertonschool.com>
2022-09-26 16:44:07 -0400

    It finally works

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,42 @@
-$(document).ready(function() {
-  $('.amenities').on('click', 'input[type="checkbox"]', function() {
-    const amenity_ids = []
+$(document).ready(function () {
+  const amenity_dict = {};
+  $('.amenities').on('click', 'input[type="checkbox"]', function () {
+    let amenity_string = '';
     if ($(this).is(':checked')) {
-      console.log($(this).data().id)
-      console.log(`.amenities ${$(this).data().id}`)
-    } else {
-      console.log()
-    }
-    if (amenity_ids.length !== 0) {
-      $('amenites.h4').text(amenity_ids)
-    }
-  })
-})
-/*$(document).ready(function() {
-    const amenityId = []
-    $('input[type=checkbox]').change(function(){
-      if ($(this.checked)){
-        amenityId = [$.attr("data-id")]
+      amenity_dict[$(this).data().id] = $(this).data().name;
+      let count = 0;
+      for (const key in amenity_dict) {
+        count++;
+        if (count === 1) {
+          amenity_string = `${amenity_dict[key]}`;
+        } else {
+          amenity_string = amenity_string + `, ${amenity_dict[key]}`;
+        }
+        if (amenity_string.length >= 25) {
+          amenity_string = amenity_string.substring(0, 25) + '...';
+          break;
+        }
       }
-    });
-    $('amenities h4').text(`${amenityId}`)
-  });*/
+      $('.amenities h4').text(amenity_string);
+    } else {
+      if (`${$(this).data().id}` in amenity_dict) {
+        delete amenity_dict[$(this).data().id];
+      }
+      let count = 0;
+      for (const key in amenity_dict) {
+        count++;
+        if (count === 1) {
+          amenity_string = `${amenity_dict[key]}`;
+        } else {
+          amenity_string = amenity_string + `, ${amenity_dict[key]}`;
+        }
+        if (amenity_string.length >= 25) {
+          amenity_string = amenity_string.substring(0, 25) + '...';
+          break;
+        }
+      }
+      $('.amenities h4').text(amenity_string);
+    }
+    console.log(amenity_dict);
+  });
+});
